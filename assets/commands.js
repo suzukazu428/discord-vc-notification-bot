c2d713b58fa06933cbda5c2a76e433412d22ff5f
suzukikazuya <ka.suzuki.428@gmail.com>
2025-01-21 15:34:34 +0900

    changeコマンドの実装

--- assets/commands.js
+++ assets/commands.js
@@ -1,46 +1,32 @@
-// import { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder, REST, Routes } from 'discord.js'
+import { SlashCommandBuilder, REST, Routes } from 'discord.js'
+import { changeNotificationChannel } from './functions.js'
 
-// const change = {
-//   data: new SlashCommandBuilder()
-//     .setName('change')
-//     .setDescription('入室を通知するテキストチャンネルを変更します。'),
+const change = {
+  data: new SlashCommandBuilder()
+    .setName('change')
+    .setDescription('入室を通知するテキストチャンネルを変更します。'),
+  async execute(interaction, outputTextChannelId) {
+    changeNotificationChannel(interaction, outputTextChannelId)
+  }
+}
+const commandDataList = [
+  change.data
+]
 
-//   async execute(interaction) {
-//     const selectMenuValueList = await createTCList(interaction.guild.channels.cache)
-//     const row = new ActionRowBuilder().setComponents(
-//       new StringSelectMenuBuilder()
-//         .setCustomId('textChannel')
-//         .setPlaceholder('テキストチャンネル')
-//         .setOptions(selectMenuValueList)
-//     )
-//     // 設定済のテキストチャンネルがない場合はembed非表示
-//     if (!outputTextChannelId) {
-//       await interaction.reply({
-//         content: '入室を通知するテキストチャンネルを選択してください。',
-//         components: [row]
-//       })
-//       return
-//     }
-//     const embed = await createEmbed(interaction, outputTextChannelId)
-//     await interaction.reply({
-//       content: '入室を通知するテキストチャンネルを選択してください。',
-//       embeds: [embed],
-//       components: [row]
-//     })
-//   }
-// }
-// const commands = [
-//   change.data
-// ]
-// try {
-//   const rest = new REST({ version: '10' }).setToken(token)
-//   await rest.put(Routes.applicationCommands(bot.user.id),{ body: commands })
-//   console.log('コマンド追加完了!')
-// } catch(e) {
-//   console.error('コマンド追加失敗', e)
-// }
-// if (interaction.commandName === 'change') {}
+const commandsInitialize = async (botID) => {
+  try {
+    const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN)
+    await rest.put(Routes.applicationCommands(botID),{ body: commandDataList })
+    console.log('コマンド追加完了!')
+  } catch(e) {
+    console.error('コマンド追加失敗', e)
+  }
+}
+const commands = {
+  commandsInitialize,
+  change
+}
 
-// export {
-//   change
-// }
+export {
+  commands
+}
--- assets/functions.js
+++ assets/functions.js
@@ -1,4 +1,4 @@
-import { EmbedBuilder } from 'discord.js'
+import { EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder } from 'discord.js'
 const createTCList = async (channels) => {
   const TC_list = channels.filter(c => c.type === 0)
   const selectMenuValueList = TC_list.map(channel => ({
@@ -15,8 +15,32 @@ const createEmbed = async (interaction, TC_ID) => {
     .setTitle(`現在の通知するテキストチャンネル: ${textChannelName}`)
   return embed
 }
+const changeNotificationChannel = async (interaction, outputTextChannelId) => {
+  const selectMenuValueList = await createTCList(interaction.guild.channels.cache)
+    const row = new ActionRowBuilder().setComponents(
+      new StringSelectMenuBuilder()
+        .setCustomId('textChannel')
+        .setPlaceholder('テキストチャンネル')
+        .setOptions(selectMenuValueList)
+    )
+    // 設定済のテキストチャンネルがない場合はembed非表示
+    if (!outputTextChannelId) {
+      await interaction.reply({
+        content: '入室を通知するテキストチャンネルを選択してください。',
+        components: [row]
+      })
+      return
+    }
+    const embed = await createEmbed(interaction, outputTextChannelId)
+    await interaction.reply({
+      content: '入室を通知するテキストチャンネルを選択してください。',
+      embeds: [embed],
+      components: [row]
+    })
+}
 
 export {
   createTCList,
-  createEmbed
+  createEmbed,
+  changeNotificationChannel
 }
